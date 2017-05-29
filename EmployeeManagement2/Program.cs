64cb56acd2f13270e6c75232bacba0dca64a07f2
Rajdeep <rajdeep@promactinfo.com>
2017-05-29 16:21:37 +0530

    Commit 1 Branch 1

--- EmployeeManagement2/Program.cs
+++ EmployeeManagement2/Program.cs
@@ -212,7 +212,7 @@ namespace Assignment2._2
     interface IMethods                                                                          //Interface
     {
         Employee Find(List<Employee> list, string id);
-        //Employee Display(Employee employee);
+        //Branch 1
     }
     public abstract class Variables
     {
